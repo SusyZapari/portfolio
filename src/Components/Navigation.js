5d2ab131f71fb25f9ea09f47c7c389032e5f1ab0
Susana Zapari <susy.zapari@gmail.com>
2019-06-13 10:14:42 -0500

    Adding Home interface

---
+++ src/Components/Navigation.js
@@ -0,0 +1,25 @@
+import React, {Componet} from 'react';
+
+
+Class Navigation extends Component {
+render (){
+    return (
+<nav>
+    <h2>Portfolio</h2>
+    <ul>
+        <li>Home</li>
+        <li>About me</li>
+        <li>Contact me</li>
+    </ul>
+</nav>
+<div>
+    Logo
+</div>
+    );
+}
+}
+
+
+
+
+export default Navigation;
