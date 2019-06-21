a537ca2bdbe5f24813d31182edc3895fe2f2a2a3
Susana Zapari <susy.zapari@gmail.com>
2019-06-21 13:28:49 -0500

    Updates

---
+++ precache-manifest.40d29ccc4b0cc4f494e5dbc093cff094.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0497b7b17959a54a1c1e1be901337fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a2301ea9930b02a9335",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "02916e45b66a35b643a4",
+    "url": "/static/css/main.35e1de02.chunk.css"
+  },
+  {
+    "revision": "5a2301ea9930b02a9335",
+    "url": "/static/js/2.699900f0.chunk.js"
+  },
+  {
+    "revision": "02916e45b66a35b643a4",
+    "url": "/static/js/main.a8498edc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fc61e7e8bbe541ba4461e9ed6c94bd2a",
+    "url": "/static/media/marcapersonal.fc61e7e8.png"
+  }
+]);
