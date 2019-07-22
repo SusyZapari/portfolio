a8a0696bfa3a3dba8c6dfdc5a74863d8f8bac0f1
Susana Zapari <susy.zapari@gmail.com>
2019-07-22 14:17:46 -0500

    Link projects from github

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -16,38 +16,40 @@ import { Card, Button, CardImg, CardTitle, CardText, CardGroup,
                                 <CardText>This is an App for a restaurant to get a better and optimic 
                                     time and service where the waiter be able to take your order (burgers 
                                     and beverages) and send it to kitchen.</CardText>
-                                <Button>View more</Button>
+                                <Button href="https://github.com/SusyZapari/gdl002-burger-queen"rel="noopnener noreferrer"target="_blank">View more</Button>
                             </CardBody>
                         </Card>
                         <Card>
                             <CardBody>
                                 <CardTitle>Veggie Happy</CardTitle>
+                                <CardImg></CardImg>
                                 <CardText>This is an App to create a social network through a cool 
                                 community for people who doesn't eat meat (Vegetarian o vegan) to have
-                                recomendations about food,restaurants, recipies, vegan places and 
+                                recomendations about food, restaurants, recipies, vegan places and 
                                 specialists as Nutritionist or Doctors.
                                 </CardText>
-                                <Button>View more</Button>
+                                <Button href="https://github.com/SusyZapari/GDL002-social-network"rel="noopener noreferrer"target="_blank">View more</Button>
                             </CardBody>
                         </Card>
                         <Card>
                             <CardBody>
-                                <CardTitle>Eye Road</CardTitle>
-                                <CardText>Road eye App is an application that is aimed at members of road 
-                                agencies. This application evaluates the indicators of injuries in the different 
-                                means of transport in the USA to be able to make impact decisions for road 
+                                <CardTitle>Road Eye</CardTitle>
+                                <CardImg></CardImg>
+                                <CardText>Road eye App is an application to evaluates the indicators of injuries 
+                                in the different kind of transport in USA to be able to make impact decisions for road 
                                 awareness programs and thus have a reference of them.</CardText>
-                                <Button>View more</Button>
+                                <Button href="https://github.com/SusyZapari/GDL002-data-lovers"rel="noopener noreferrer"target="_blank">View more</Button>
                             </CardBody>
                         </Card>
                         <Card>
                             <CardBody>
                                 <CardTitle>Memoraland</CardTitle>
+                                <CardImg></CardImg>
                                 <CardText>Memoraland is a traditional memory game in 2D, where you should 
                                 identify two equal figures to obtain pairs.
                                 It is a simple and once entertaining game thought mainly for the early 
                                 stages of life, where children can learn at the same time to play.</CardText>
-                                <Button>View more</Button>
+                                <Button href="https://github.com/SusyZapari/GDL002-mobile-game"rel="noopener noreferrer"target="_blank">View more</Button>
                             </CardBody>
                         </Card>
                     </CardGroup>
